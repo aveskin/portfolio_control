afbc4dd0d8033917f0029abe34afc96140b15e04
aveskin <job.eskin@yandex.ru>
2024-11-19 21:10:54 +0300

    initial

---
+++ src/test/java/ru/aveskin/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aveskin.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
